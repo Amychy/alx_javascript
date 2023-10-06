23704385a33ceaf6edd078fc325a5676b5231486
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 19:48:00 +0100

    Update 103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,12 +1,22 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12
+  value: 12,
 };
+
 console.log(myObject);
 
-myObject.incr = new Function('', 'this.value++;');
-myObject.incr();
+myObject.incr = function () {
+  myObject.value++;
+};
+
+Object.defineProperty(myObject, 'incr', {
+  enumerable: false, // Prevent the function from being included in the output
+  value: '[Function]', // Replace the function with '[Function]' in the output
+});
+
+myObject.incr(); // Increment the value
+
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
